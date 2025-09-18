1cf82d69301f7d5b3252ab99d6a1a1b4a43e90c2
Matheus Santos <156011402+mathessantos@users.noreply.github.com>
2025-09-18 15:25:44 -0300

    Delete MatheusOO/base_project directory

--- MatheusOO/base_project/src/main/java/br/edu/ifpr/pgua/eic/tads/controllers/CadastroController.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpr.pgua.eic.tads.controllers;
-import io.javalin.http.Handler;
-
-public class CadastroController {
-    public Handler get = ctx -> {
-        ctx.redirect("cadastro.html");
-    };
-}
--- MatheusOO/base_project/src/main/java/br/edu/ifpr/pgua/eic/tads/controllers/IndexController.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpr.pgua.eic.tads.controllers;
-import io.javalin.http.Handler;
-
-public class IndexController {
-    public Handler get = ctx -> {
-        ctx.redirect("index.html");
-    };
-}
